c801148e4609aba23b0278ec42dd55e7bf9d22f3
WebMohit20 <2003mohitkhemani@gmail.com>
2024-10-14 06:15:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 //your JS code here. If required.
+let bands = ['The Plot in You',
+			   'The Devil Wears Prada',
+			   'Pierce the Veil',
+			   'Norma Jean',
+			   'The Bled',
+			   'Say Anything',
+			   'The Midway State',
+			   'We Came as Romans',
+			   'Counterparts',
+			   'Oh, Sleeper',
+			   'A Skylit Drive',
+			   'Anywhere But Here',
+			   'An Old Dog'];
+
+function Has(a,b){
+	let art = ["A","An","The"];
+	
+	let arr=a.split(" ");
+	for(let t of art){
+	if(arr.includes(t)){
+		console.log(arr)
+	}
+	}
+		// if(
+}
+
+let sorted = bands.sort((a,b)=>{
+	
+	Has(a)
+})
+
+
+
+
+
+
+
+
+
